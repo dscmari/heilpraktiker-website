b1583d301cba50f214489fbb1f1c1b8d327e56cd
Marian Noekel <marian@MacCarlo.fritz.box>
2024-03-01 12:17:02 +0100

    implement mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,40 @@ import styled from 'styled-components';
 import logo_dummy from "../assets/imgs/logo_dummy.png";
 
 const Container = styled.nav`
-    margin: 1rem;
     display: flex;
+    flex-direction: column;
+
+    @media (min-width: 768px) {
+    margin: 1rem;
+    flex-direction: row;
+    justify-content: center;
+  }
+`
+
+const NavigationHeader = styled.div`
+    display: flex;
+    justify-content: space-around;
     align-items: center;
+
+    .nav-header-item{
+        margin: 1rem;
+    }
+
+    @media (min-width: 768px) {
+    #burger-menu{
+        display: none;
+    }
+  }
+    
 `
 
 const Navigation = styled.ul`
-    list-style: none;
     display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 1rem;
+    border-radius: 5px;
+    list-style: none;
     gap: 1.5rem;
     margin: 1rem;
 
@@ -27,14 +53,24 @@ const Navigation = styled.ul`
     a.active{
         text-decoration:underline;
     }
+
+    @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `
 
 function Navbar(){
     return (
         <Container>
-            <Link to="/">
-                <img src={logo_dummy} alt="logo" style={{ height: "15vh" }} />
-            </Link>
+            <NavigationHeader style={{display:"flex", justifyContent: "space-between"}}>
+                <Link className="nav-header-item" to="/">
+                    <img src={logo_dummy} alt="logo" style={{ height: "15vh" }} />
+                </Link>
+                <div id="burger-menu" className="nav-header-item">
+                    <h1>Burger menu</h1>
+                </div>
+            </NavigationHeader>
+            
             {/* TODO
                 preventScrollReset anstatt helper function um zum seitenanfang zu navigieren (https://medium.com/@alexanie_/link-component-in-react-router-6872291bf78e)*/}
             <Navigation>
