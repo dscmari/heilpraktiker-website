6a103ffdc05485a851cea84ea8ee7b57208ffe22
Marian Noekel <marian@MacCarlo.local>
2024-03-15 22:33:58 +0100

    update NotFound view

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function NotFound() {
     return (
         <div>
-            <h1>Oops! You seem to be lost.</h1>
-            <p>Here are some helpful links:</p>
+            <p>Oops! Dieser Navigationspunkt wurde noch nicht erstellt.</p>
+            <p>Erstellte Navigationspunkte:</p>
             <div>
             <Link to='/'>Home</Link>
             </div>
